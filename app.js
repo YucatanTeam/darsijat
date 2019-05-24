262357ca1a7227d0f78f400afe8ef44a1705c996
eaNODE <ea_pain@yahoo.com>
2019-05-24 18:45:39 +0800

    fixed bug

--- app.js
+++ app.js
@@ -19,20 +19,20 @@ bot.use(session({
 ))
 
 bot.start(function(ctx){
-    
-    
     con.query(`SELECT * FROM user WHERE mId = ?` , [`${ctx.message.from.id}`],function(err,row){ 
-      if(Object.entries(row).length === 0 && ctx.message.from.id != process.env.ADMIN_ID){ 
-          con.query(`INSERT INTO user(mId) VALUES(?)`,[ctx.message.from.id])
+      if(Object.entries(row).length === 0){
+        // user use this bot for the first time
+        con.query(`INSERT INTO user(mId) VALUES(?)`,[ctx.message.from.id])
+      } else if(ctx.message.from.id != process.env.ADMIN_ID){ 
+        // user already exists and is not admin
           ctx.session.isAdmin = false
-          ctx.reply('`HELLO USER :)`')
-      }
-      else{
+          ctx.reply('`از منوی زیر برای خرید جزوه های موجود استفاده کنید`')
+      } else{
+        // user id admin
         ctx.session.isAdmin = true
-        ctx.reply(`HELLO ADMIN :)`)
+        ctx.reply(`از منوی زیر برای تنظیم ربات خود استفاده کنید`)
       }
     })
-
 });
 
 //launch server
