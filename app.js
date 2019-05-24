8e1db8f70bab1125f1fac4076bfdbf127acb427c
eaNODE <ea_pain@yahoo.com>
2019-05-24 18:24:37 +0800

    updated server

--- app.js
+++ app.js
@@ -2,8 +2,21 @@
 require('dotenv').config()
 
 var telegraf = require ('telegraf');
+const session = require('telegraf/session')
 const bot = new telegraf(process.env.TOKEN);
 const con = require('./config/db');
+
+bot.use(session({
+    getSessionKey: (ctx) => {
+      if (ctx.from && ctx.chat) {
+        return `${ctx.from.id}:${ctx.chat.id}`
+      } else if (ctx.from && ctx.inlineQuery) {
+        return `${ctx.from.id}:${ctx.from.id}`
+      }
+      return null
+    }
+  }
+))
 
 bot.start(function(ctx){
     
@@ -11,16 +24,16 @@ bot.start(function(ctx){
     con.query(`SELECT * FROM user WHERE mId = ?` , [`${ctx.message.from.id}`],function(err,row){ 
       if(Object.entries(row).length === 0 && ctx.message.from.id != process.env.ADMIN_ID){ 
           con.query(`INSERT INTO user(mId) VALUES(?)`,[ctx.message.from.id])
-        }
+          ctx.session.isAdmin = false
+          ctx.reply('`HELLO USER :)`')
+      }
+      else{
+        ctx.session.isAdmin = true
+        ctx.reply(`HELLO ADMIN :)`)
+      }
     })
-    ctx.reply(`test commands:
-    /motevassete
-    /ebtedaii`)
-});
 
-bot.command("motevassete",function(ctx) {
-    ctx.reply("holy momy fucking shit :)")
-})
+});
 
 //launch server
 bot.launch();
