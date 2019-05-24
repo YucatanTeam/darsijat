e0e221a068e62aee2c627ae1cbd4b86906d2f833
eaNODE <ea_pain@yahoo.com>
2019-05-24 17:09:57 +0800

    sql and dotenv updated

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
 
 require('dotenv').config()
-// make a .env file
-// DB_HOST=localhost
-// DB_NAME=kheibot
-// DB_USER=root
-// DB_PASS=root
-// PORT=3000
-// TOKEN=780218594:AAFYGdHotytGkkBBE37C19YvKqFgIWR7zWs
 
 var telegraf = require ('telegraf');
 const bot = new telegraf(TOKEN);
 const con = require('./config/db');
-
-//on start text
-
 
 bot.start(function(ctx){
     
@@ -29,7 +19,4 @@ bot.start(function(ctx){
 
 });
 
-
-
-//launch server
 bot.launch();
--- sql/kheibot.sql
+++ sql/kheibot.sql
@@ -5,13 +5,22 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+DROP TABLE IF EXISTS `transactions`;
+CREATE TABLE `transactions` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(8) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int(8) NOT NULL AUTO_INCREMENT,
-  `isPayed` int(8) NOT NULL,
   `mId` varchar(16) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
--- 2019-05-24 01:01:13
+-- 2019-05-24 09:03:54
