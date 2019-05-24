a36d2e6d0ab24e26d2cd24e4e248425525680e6d
eaNODE <ea_pain@yahoo.com>
2019-05-24 20:17:48 +0800

    working on inline keyboard

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ require('dotenv').config()
 
 var telegraf = require ('telegraf');
 const session = require('telegraf/session')
+const TelegrafInlineMenu = require('telegraf-inline-menu')
 const bot = new telegraf(process.env.TOKEN);
 const con = require('./config/db');
+const menu = new TelegrafInlineMenu(ctx => `سلام ${ctx.message.from.username}!`)
+menu.setCommand('start')
+menu.simpleButton('I am excited!', 'a', {
+  doFunc: ctx => ctx.reply('As am I!')
+})
 
+bot.use(menu.init())
 bot.use(session({
     getSessionKey: (ctx) => {
       if (ctx.from && ctx.chat) {
@@ -23,18 +30,14 @@ bot.start(function(ctx){
       if(Object.entries(row).length === 0){
         // user use this bot for the first time
         con.query(`INSERT INTO user(mId) VALUES(?)`,[ctx.message.from.id])
-        ctx.session.isAdmin = false
       } else if(ctx.message.from.id != process.env.ADMIN_ID){ 
         // user already exists and is not admin
           ctx.session.isAdmin = false
-          ctx.reply('`از منوی زیر برای خرید جزوه های موجود استفاده کنید`')
       } else{
         // user id admin
         ctx.session.isAdmin = true
-        ctx.reply(`از منوی زیر برای تنظیم ربات خود استفاده کنید`)
       }
     })
 });
 
-//launch server
-bot.launch();
+bot.startPolling()
--- sql/kheibot.sql
+++ sql/kheibot.sql
@@ -26,9 +26,22 @@ CREATE TABLE `transactions` (
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int(8) NOT NULL AUTO_INCREMENT,
-  `mId` varchar(16) NOT NULL,
+  `mId` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
--- 2019-05-24 09:32:41
+DROP TABLE IF EXISTS `user_trs`;
+CREATE TABLE `user_trs` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(8) NOT NULL,
+  `transactions_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `transactions_id` (`transactions_id`),
+  CONSTRAINT `user_trs_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `user_trs_ibfk_2` FOREIGN KEY (`transactions_id`) REFERENCES `transactions` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- 2019-05-24 11:39:34
