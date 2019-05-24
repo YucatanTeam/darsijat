15edecb4a6de52f4f50369ab970e3ed08125deee
eaNODE <ea_pain@yahoo.com>
2019-05-24 17:40:46 +0800

    sql updated

--- sql/kheibot.sql
+++ sql/kheibot.sql
@@ -5,6 +5,14 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+DROP TABLE IF EXISTS `commands`;
+CREATE TABLE `commands` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `command` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
 DROP TABLE IF EXISTS `transactions`;
 CREATE TABLE `transactions` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -23,4 +31,4 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
--- 2019-05-24 09:03:54
+-- 2019-05-24 09:32:41
