fefc10f8b3263087d608420625048cfdc63a03e0
eaNODE <ea_pain@yahoo.com>
2019-05-25 01:01:41 +0800

    updated

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
 
 require('dotenv').config()
-
 var telegraf = require ('telegraf');
-const session = require('telegraf/session')
-const TelegrafInlineMenu = require('telegraf-inline-menu')
 const bot = new telegraf(process.env.TOKEN);
 const con = require('./config/db');
+const session = require('telegraf/session')
 
-const mainMenu = new TelegrafInlineMenu(ctx => `سلام ${ctx.message.from.username}!`)
-const fooMenu = new TelegrafInlineMenu('Foo Menu')
-const barMenu = new TelegrafInlineMenu('Bar Menu')
-mainMenu.setCommand('start')
-mainMenu.submenu('Open Foo Menu', 'foo', fooMenu)
-fooMenu.submenu('Open Bar Menu', 'bar', barMenu)
-barMenu.simpleButton('Hit me', 'something', {
-  doFunc: ctx => ctx.reply('As am I!')
-})
-
-bot.use(mainMenu.init())
 bot.use(session({
     getSessionKey: (ctx) => {
       if (ctx.from && ctx.chat) {
@@ -30,19 +17,8 @@ bot.use(session({
   }
 ))
 
-bot.start(function(ctx){
-    con.query(`SELECT * FROM user WHERE mId = ?` , [`${ctx.message.from.id}`],function(err,row){ 
-      if(Object.entries(row).length === 0){
-        // user use this bot for the first time
-        con.query(`INSERT INTO user(mId) VALUES(?)`,[ctx.message.from.id])
-      } else if(ctx.message.from.id != process.env.ADMIN_ID){ 
-        // user already exists and is not admin
-          ctx.session.isAdmin = false
-      } else{
-        // user id admin
-        ctx.session.isAdmin = true
-      }
-    })
+bot.start((ctx)=>{
+  ctx.reply('Welcome')
 });
 
 bot.startPolling()
