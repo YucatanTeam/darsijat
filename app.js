b5cb73f7412701c0344a578bce3e0ea795f22fa3
eaNODE <ea_pain@yahoo.com>
2019-05-26 03:49:10 +0800

    tested with ngrok

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const express = require('express');
 const body = require('body-parser');
 const path = require('path');
 var telegraf = require ('telegraf');
-const session = require('telegraf/session')
-const TelegrafInlineMenu = require('telegraf-inline-menu')
 const bot = new telegraf(process.env.TOKEN);
 const con = require('./config/db');
 
@@ -58,7 +56,8 @@ app.get("/files", auth, (req, res) => {
 app.get("/file/:fid/:uid", (req, res) => {
   con.query("SELECT * FROM transactions WHERE order_id = ? AND verify = ?", [`${req.params.fid}.${req.params.uid}`, 0], (err, row)=>{
     if(row.length){
-      // TODO send file to user from bot without payment process
+      // TODO
+      console.log("send file to user from bot without payment process...")
     } else { // payment process
       con.query("SELECT * FROM files WHERE id = ? ", [req.params.fid], (err, row)=>{
         if(row.length){
@@ -68,8 +67,8 @@ app.get("/file/:fid/:uid", (req, res) => {
             url: 'https://api.idpay.ir/v1.1/payment',
             headers: {
               'Content-Type': 'application/json',
-              'X-API-KEY': '6a7f99eb-7c20-4412-a972-6dfb7cd253a4', // TODO
-              'X-SANDBOX': 1, // TODO
+              'X-API-KEY': process.env.APITOKEN,
+              'X-SANDBOX': 1,
             },
             body: {
               'order_id': order_id,
@@ -82,8 +81,10 @@ app.get("/file/:fid/:uid", (req, res) => {
           
           request(options, function (error, response, body) {
             if (error) {
+              console.log(error)
               res.send("به درگاه متصل نشد!")
             } else{
+              console.log(body.error_message)
               if(body.error_code) res.send("به درگاه متصل نشد!")
               else{
                 con.query("INSERT INTO transactions(id, order_id, amount, verify) VALUES(?,?,?,?) ", [body.id, order_id, row[0].amount, 0], (err, row)=>{
@@ -105,13 +106,13 @@ app.get("/file/:fid/:uid", (req, res) => {
 })
 
 app.get("/callback", (req, res)=>{
-    // TODO redirect to bot
+    console.log("redirect to bot..")
 })
 
-// we are talking to bank..... :)
+// bank <-> idpay <-> we ..... :)
 app.post("/callback", (req, res) => {
-
-  if(req.body.status == 100){
+  // console.log(req.body.status)
+  if(req.body.status == 10){
       
       const bodyit = {
         'id': req.body.id,
@@ -123,8 +124,8 @@ app.post("/callback", (req, res) => {
         url: 'https://api.idpay.ir/v1.1/payment/verify',
         headers: {
           'Content-Type': 'application/json',
-          'X-API-KEY': '6a7f99eb-7c20-4412-a972-6dfb7cd253a4', // TODO
-          'X-SANDBOX': 1, // TODO
+          'X-API-KEY': process.env.APITOKEN,
+          'X-SANDBOX': 1,
         },
         body: bodyit,
         json: true,
@@ -137,13 +138,16 @@ app.post("/callback", (req, res) => {
           if(body.error_code){
             res.end()
           } else {
-            // TODO bot send file with track_id and other infos
+            // TODO
+            console.log("bot send file with track_id and other infos...")
             con.query("UPDATE transactions SET status = ?, track_id = ?, card_no = ?, hash_card_no = ?, date = ?, verify = ? WHERE order_id = ?",
-            [body.status, body.track_id+"."+body.payment.track_id,body.payment.card_no, body.payment.hash_card_no, body.verify.date, 1, req.params.ufid], (err, row)=>{
+            [body.status, body.track_id+"."+body.payment.track_id,body.payment.card_no, body.payment.hash_card_no, body.verify.date, 1, req.body.order_id], (err, row)=>{
               if(err) {
-                // TODO bot tell khei to pm to dev
+                // TODO
+                console.log("bot tell khei to pm to dev")
               }
               res.end()
+              // console.log(row)
             })
           }
         }
