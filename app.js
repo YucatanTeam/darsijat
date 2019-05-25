ff00173cb075c6b01e4f17a05d7d23bdf936940d
khashayar1375 <khashayar.mafi75@gmail.com>
2019-05-25 17:08:44 +0430

    test commands

--- app.js
+++ app.js
@@ -7,19 +7,22 @@ const con = require('./config/db');
 
 bot.start(function(ctx){
     
-    console.log(typeof(ctx.message.from.id))
+    
     con.query(`SELECT * FROM user WHERE mId = ?` , [`${ctx.message.from.id}`],function(err,row){     
-      if(Object.entries(row).length === 0){
-        con.query(`INSERT INTO user(isPayed,mId) VALUES(?,?)`,[0,ctx.message.from.id])
-      }
-      else{
-  
-      }
+        for (var i in row){
+          if(row[i].mId != ctx.message.from.id){
+            con.query(`INSERT INTO user(isPayed,mId) VALUES(?,?)`,[0,ctx.message.from.id])
+          }
+        }
     })
-
+    ctx.reply(`test commands:
+    /motevassete
+    /ebtedaii`)
 });
 
-
+bot.command("motevassete",function(ctx) {
+    ctx.reply("holy momy fucking shit :)")
+})
 
 //launch server
 bot.launch();
