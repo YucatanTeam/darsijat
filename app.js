f8e5698ab0692d2fb1292ab03937495c1fdc8986
force1267 <force1267@gmail.com>
2019-05-25 16:50:12 +0430

    .env TOKEN

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
+
+require('dotenv').config()
+// make a .env file
+// DB_HOST=localhost
+// DB_NAME=kheibot
+// DB_USER=root
+// DB_PASS=root
+// PORT=3000
+// TOKEN=780218594:AAFYGdHotytGkkBBE37C19YvKqFgIWR7zWs
+
 var telegraf = require ('telegraf');
-const bot = new telegraf('780218594:AAFYGdHotytGkkBBE37C19YvKqFgIWR7zWs');
+const bot = new telegraf(TOKEN);
 const con = require('./config/db');
 
 //on start text
