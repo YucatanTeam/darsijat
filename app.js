fda3ad9449288f95954292508931349a1ace3518
force1267 <force1267@gmail.com>
2019-05-26 02:48:16 +0430

    express logic routes

--- app.js
+++ app.js
@@ -28,9 +28,9 @@ bot.launch();
 
 const app = express();
 
-
-
+const USERNAME = process.env.USERNAME;
 var PASSWORD = process.env.PASSWORD;
+
 app.get("/admin", auth, express.static(path.join(__dirname, "admin")));
 app.get("/login", express.static(path.join(__dirname, "login")));
 app.post("/login", auth, req => req.res.json({status: 1}));
@@ -40,11 +40,37 @@ app.post("/changePassword", auth, req => {
   // TODO telegram notify admin that password has changed
 })
 
+app.post("/file", auth, (req, res) => {
+  // upload the file and tags
+  // save the file and rename
+  // add to db
+})
+
+app.get("/files", auth, (req, res) => {
+  // list of all files
+})
+
+app.get("/file/:uid", (req, res) => {
+  // if user(uid) bought redirect to bot and resend file
+  // else redirect to idpay with callback /callback/:uid
+})
+
+app.get("/callback/:uid", (req, res) => {
+  // if status = fail set transaction.status = 0
+  // and bot.send failure to user(uid)
+  // else set transaction.status = 1
+  // and bot.send file to user(uid)
+})
+
+
 
 app.listen(process.env.PORT);
 
+// TODO use passport
 function auth(req, res, next) {
-  if(req.body.username === "admin" && req.body.password === PASSWORD) {
+  if(req.body.username === USERNAME && req.body.password === PASSWORD) {
+    next();
+  } else if(req.query.u === USERNAME && req.query.p === PASSWORD) {
     next();
   } else {
     res.redirect("/login")
