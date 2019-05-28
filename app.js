5443b607c79fa0b0a82f240afd4ac6749e677470
eaNODE <ea_pain@yahoo.com>
2019-05-28 14:50:22 +0800

    handling not found issue

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ con.query("SELECT * FROM tags;", (err, rows) => {
   });
 });
 
- bot.start((ctx)=> ctx.reply(`به بات جستجوی جزوه خوش آمدید,
+bot.start((ctx)=> ctx.reply(`به بات جستجوی جزوه خوش آمدید,
                                کلیدواژه های خود را وارد کرده
                               تا جزوه مورد نظر خود را برای خرید پیدا کنید!
                              `))
@@ -210,7 +210,15 @@ app.get("/file/:fid/:uid", (req, res) => {
               filename: `${fi[0].descr}.pdf`
             },[{caption:`${tr.track_id}`}]).catch(console.log);
         } else{
-          res.status(404).send("فایل مورد نظر پیدا نشد!") // TODO tell user pm admin for the file
+          // tell user pm admin for the file , perhaps admin delete the file
+          // and user want to take it cause she/he paid for it before!
+          telegram.sendMessage(req.params.uid,`
+              فایل مورد نظر پیدا نشد,
+              لطفا با ادمین تماس بگیرید.
+              این جزوه خریده داری شده است ! کد رهگیری شما :
+          `, [{parse_mode: `<strong>${tr.track_id}</strong>`}]
+            ).catch(console.log)
+          res.status(404).send("فایل مورد نظر پیدا نشد!")
         }
       })
     } else { // payment process
